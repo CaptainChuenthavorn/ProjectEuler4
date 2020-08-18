659c24dbc91af8938388bb6145705f6b22eef8f8
จักรภัทรณ์ ชื่นถาวร <captainchuenthavorn@hotmail.com>
2020-08-18 13:58:26 +0700

    add user interface and input from user

--- ProjectEuler4/LargestPalindrome.cpp
+++ ProjectEuler4/LargestPalindrome.cpp
@@ -2,15 +2,22 @@
 #include<stdio.h>
 int main() {
 	int x, y;
-	int product, revProd, Product,max,keepX,keepY;
-	for (x = 100;x < 1000; x++) {
-		for (y = 100; y < 1000;y++) {
+	int product, revProd,num1,num2, Product,max,keepX,keepY;
+	max = 0;
+	keepX = 0;
+	keepY = 0;
+	printf("Program Find the largest palindome number from a * b  \nEnter a : ");
+	scanf("%d", &num1);
+	printf("Enter b : ");
+	scanf("%d", &num2);
+	for (x = 100;x <= num1; x++) {
+		for (y = 100; y <= num2;y++) {
 			product = x * y;
 			Product = product;
 			revProd = 0;
 				while (revProd >= 0)
 				{
-					//ขั้นตอนสลับเลขหน้าหลัง
+					//logic method
 					revProd = revProd * 10 + (Product % 10);
 					Product /= 10;
 					
@@ -26,7 +33,12 @@ int main() {
 				}
 		}
 	}
-	printf("%d x %d = %d\n", keepX, keepY, max);
-	printf("Largest Product is : %d",max);
+	if (max == 0) {
+		printf("Not have palindome number in this a * b");
+	}
+	else {
+		printf("%d x %d = %d\n", keepX, keepY, max);
+		printf("Largest palindome number is : %d", max);
+	}
 	return 0;
 }
